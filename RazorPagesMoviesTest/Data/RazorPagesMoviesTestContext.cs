6a7b7cc621102bc0ca4ba0556dc7cf3a4da4703a
DESKTOP-B37R6OQ\Doom <Ljviljoen2020>
2021-02-15 12:10:20 +0200

    Update

---
+++ RazorPagesMoviesTest/Data/RazorPagesMoviesTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMoviesTest.Data
+{
+    public class RazorPagesMoviesTestContext : DbContext
+    {
+        public RazorPagesMoviesTestContext (DbContextOptions<RazorPagesMoviesTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
