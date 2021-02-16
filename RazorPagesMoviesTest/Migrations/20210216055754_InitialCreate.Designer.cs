766d4df00289699db4e2f416c183396d760ffb4d
DESKTOP-B37R6OQ\Doom <Ljviljoen2020>
2021-02-16 08:44:11 +0200

    Update and Done

--- RazorPagesMoviesTest/Migrations/20210216055754_InitialCreate.Designer.cs
+++ RazorPagesMoviesTest/Migrations/20210216055754_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using RazorPagesMoviesTest.Data;
 namespace RazorPagesMoviesTest.Migrations
 {
     [DbContext(typeof(RazorPagesMoviesTestContext))]
-    [Migration("20210216031113_InitialCreate")]
+    [Migration("20210216055754_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -29,19 +29,23 @@ namespace RazorPagesMoviesTest.Migrations
                         .UseIdentityColumn();
 
                     b.Property<string>("Genre")
-                        .HasColumnType("nvarchar(max)");
+                        .IsRequired()
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)");
 
                     b.Property<decimal>("Price")
                         .HasColumnType("decimal(18,2)");
 
                     b.Property<string>("Rating")
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(5)
+                        .HasColumnType("nvarchar(5)");
 
                     b.Property<DateTime>("ReleaseDate")
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Title")
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(60)
+                        .HasColumnType("nvarchar(60)");
 
                     b.HasKey("ID");
 
--- RazorPagesMoviesTest/Migrations/20210216055754_InitialCreate.cs
+++ RazorPagesMoviesTest/Migrations/20210216055754_InitialCreate.cs
@@ -13,11 +13,11 @@ namespace RazorPagesMoviesTest.Migrations
                 {
                     ID = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
                     ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
                     Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    Rating = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    Rating = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: true)
                 },
                 constraints: table =>
                 {
